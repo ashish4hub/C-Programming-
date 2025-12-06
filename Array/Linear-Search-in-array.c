3c21846cebb990e7db41026fb1632f80bf51a604
ashish4hub <ashish.k620045@gmail.com>
2025-12-06 17:44:33 +0530

    Update Linear-Search-in-array.c

--- Array/Linear-Search-in-array.c
+++ Array/Linear-Search-in-array.c
@@ -9,21 +9,21 @@ int main (){
     printf("Enter %d elements:\n",n);
 
     //Storing the input array
-    for(i=0;i<n;i++){
+    for(i = 0; i < n; i++){
         scanf("%d",&x[i]);
     }
     printf("Enter the key element to search: ");
     scanf("%d",&key);
 
     //Linear search
-    for(i=0;i<n;i++){
-        if(x[i]==key){
-            found=1;
+    for(i = 0; i < n; i++){
+        if(x[i] == key){
+            found = 1;
             break;
         }
     }
-    if(found==1){
-        printf("Element %d found at position %d in the defined array",key,i+1);
+    if(found == 1){
+        printf("Element %d found at position %d in the defined array",key,i + 1);
     }
     else{
         printf("Not found ! No such element is detected in the defined array");
