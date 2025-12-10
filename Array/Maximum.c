6012ff4a63d4dbe85060ae6f8ea265b88c911ba6
ashish4hub <ashish.k620045@gmail.com>
2025-12-11 01:43:21 +0530

    Update Maximum.c

--- Array/Maximum.c
+++ Array/Maximum.c
@@ -1,7 +1,7 @@
 //Program: To find the maximum number in an array of numbers.
 #include<stdio.h>
 int main() {
-int n[8]={3,11,7,9,2,15,25,32},v=0;
+int n[8]={3,11,7,9,2,15,25,32};
 int max=n[0];
 for(int i=0;i<8;i++){
 if(n[i]>max){
